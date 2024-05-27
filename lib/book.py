ef69f743e15f600eea27c6aa3954970b3b26b549
Stephen Ochieng <materiamystic@gmail.com>
2024-05-28 00:46:13 +0300

    Adds required properties

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count) -> None:
+        self.title = title
+        self.page_count = page_count
     
         
+    @property
+    def page_count(self):
+        print("page_count getter called")
+       
+        return self._page_count
+        
+    @page_count.setter
+    def page_count(self, value):
+        if type(value) in (int,):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+            
+            
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
